be968eba9f52ddb7a7c504db86c636ceef0408e7
MrxBlackWolf <nikola.milicevic.169.12@ict.edu.rs>
2016-02-08 20:58:32 +0100

    upis radi

--- registracija.php
+++ registracija.php
@@ -1,12 +1,14 @@
 <?php
 
+$rezultat = "";
+
 if (isset($_POST['napravi'])) {
 	$korIme = $_POST['korisnicko_ime'];
 	$lozinka = $_POST['lozinka'];
 	$email = $_POST['email'];
 	$id_uloga['id_uloga'];
-	
-	$upit="INSERT into korisnik VALUES('', $korIme', '$lozinka' '$email', '$id_uloga')";
-	$rezultat=  mysql_query($upit,$konekcija);
-	}
+
+	$upit = "INSERT into korisnik VALUES('', $korIme', '$lozinka' '$email', '$id_uloga')";
+	$rezultat = mysql_query($upit, $konekcija);
+}
 
