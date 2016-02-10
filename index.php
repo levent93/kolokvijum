6985632a12a98e67f1bcac84f17a8ec56d36e3f0
MrxBlackWolf <nikola.milicevic.169.12@ict.edu.rs>
2016-02-11 00:54:18 +0100

    Siso jedna

--- index.php
+++ index.php
@@ -25,7 +25,7 @@ require_once './registracija.php';
 		<section id="banner">
 			<div class="inner split">
 				<section>
-					<h2 onclick="loadDoc();">Trenutno vreme sada</h2>
+					<h2 onclick="loadDoc();">MJUSTAFA PJEDERU NE MENJAJ MOJE FAJLOVE</h2>
 					<p id="timer"></p>
 					<input type="button" id="prikazi" class="button special" value="Prikazi" onclick="prikazi(1, 0);">
 					<input type="button" id="sakri" class="button special" value="Sakri" onclick="prikazi(0, 0);">
